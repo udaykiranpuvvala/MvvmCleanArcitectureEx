ffa9c7d7db42c106f8f32e87e0485a90c01f85cf
Uday Kiran <uday.puvvala@ierl.in>
2022-10-20 17:23:32 +0530

    159- Fragments & Navigation Component session changes

--- app/src/main/java/com/example/mvvmcleanarchitectureex2/MainActivity.kt
+++ app/src/main/java/com/example/mvvmcleanarchitectureex2/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.mvvmcleanarchitectureex2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mvvmcleanarchitectureex2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
